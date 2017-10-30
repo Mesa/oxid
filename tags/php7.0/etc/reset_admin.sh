c90e8f2d5e4adbce67fb825b4454cf17da3989be
Mesa <daniel.langemann@gmx.de>
2017-10-31 00:07:31 +0100

    Removed obsolete params and use ENV vars for password reset

--- tags/php7.0/etc/reset_admin.sh
+++ tags/php7.0/etc/reset_admin.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Reset admin password to value of environment variable [OXID_ADMIN_PASSWORD]."
-echo "To change value update you docker-compose.yml"
+echo "Resetting username and password to environment variables from docker-compose.yml"
 
 NEW_SALT=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
 
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "
+mysql -e "
     UPDATE oxuser
     SET OXPASSWORD = MD5( CONCAT( '${OXID_ADMIN_PASSWORD}',  '${NEW_SALT}' ) ) ,
         OXPASSSALT = hex('${NEW_SALT}'),
